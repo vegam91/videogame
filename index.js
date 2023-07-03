960e4ee71a03b444fa19e660490d61a3b83fd1cc
vegam91 <vegamartinoj@gmail.com>
2023-07-03 19:05:39 +0200

    fix:indexjs, bso

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import Game from "./js/game.js"
+import Game from './js/game.js'
+
 addEventListener('load', () => {
 	const startButton = document.querySelector('.start-screen button')
 
-	
+	startButton.addEventListener('click', () => {
+		document.querySelector('.start-screen').classList.add('hidden')
 
 		Game.init()
 	})
+})
